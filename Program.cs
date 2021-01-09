5e5683b882551c28f9e37f6405d9c19af367bb87
liu <lsjsoft@163.com>
2021-01-09 12:11:31 +0800

    1

--- Program.cs
+++ Program.cs
@@ -23,6 +23,24 @@ namespace UnityCsProjGen
 
         static int Main(string[] args)
         {
+            try
+            {
+                int ret = ProcessMain(args);
+                if (ret < 0)
+                {
+                    Console.ReadKey();
+                }
+                return ret;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -101;
+            }
+        }
+
+        static int ProcessMain(string[] args)
+        {
             projectGuid = Guid.NewGuid().ToString();
 
             if (args.Length <= 1)
@@ -41,6 +59,12 @@ namespace UnityCsProjGen
                 UnityWorkDir += "\\";
             }
 
+            if (!System.IO.Directory.Exists(UnityWorkDir))
+            {
+                Console.WriteLine("Dir: {0} NotExists", UnityWorkDir);
+                return -201;
+            }
+
             var templateName = "Assembly-CSharp-Editor.csproj";
             if (args.Length > 2)
             {
@@ -50,59 +74,66 @@ namespace UnityCsProjGen
             var outputFilename = System.IO.Path.Combine(UnityWorkDir, SourceFolderName + ".csproj");
             var templateFilename = System.IO.Path.Combine(UnityWorkDir, templateName);
 
+            var slnFname = System.IO.Path.GetFileNameWithoutExtension(UnityWorkDir.Substring(0, UnityWorkDir.Length - 1)) + ".sln";
+            var slnFilename = System.IO.Path.Combine(UnityWorkDir, slnFname);
+            if (!System.IO.File.Exists(slnFilename))
+            {
+                Console.WriteLine("File Not Exists: {0}", slnFilename);
+                return -5;
+            }
+
             if (!System.IO.File.Exists(templateFilename))
             {
                 Console.WriteLine("FileNotExists:{0}", templateFilename);
                 return -2;
             }
 
-            if (!Execute(UnityWorkDir, SourceFolderName, templateFilename, outputFilename))
+            List<string> slnContents = new List<string>();
+            slnContents.AddRange(System.IO.File.ReadAllLines(slnFilename));
+
+            if (FindIndexOf(slnContents, SourceFolderName) > 0)
+            {
+                Console.WriteLine("Already has: ", templateFilename);
+                return -6;
+            }
+
+            if (!ExecuteReplaceAction(UnityWorkDir, SourceFolderName, templateFilename, outputFilename))
             {
                 return -3;
             }
 
-            var slnFname = System.IO.Path.GetFileNameWithoutExtension(UnityWorkDir.Substring(0, UnityWorkDir.Length-1)) + ".sln";
-            var slnFilename = System.IO.Path.Combine(UnityWorkDir, slnFname);
+            var acecsprojh = FindIndexOf(slnContents, "Assembly-CSharp-Editor.csproj");
+            if (acecsprojh > 0)
+            {
+                var ProjectConfig = slnContents[acecsprojh];
+                var guid = PickGuid(ProjectConfig);
+                var insertProj = string.Format("Project(\"{{{0}}}\") = \"{1}\", \"{1}.csproj\", \"{{{2}}}\"", guid, SourceFolderName, projectGuid);
+                slnContents.Insert(acecsprojh + 2, insertProj);
+                slnContents.Insert(acecsprojh + 3, "EndProject");
+            }
+
+            var gpIndex = FindIndexOf(slnContents, "GlobalSection(ProjectConfigurationPlatforms)");
+            if (gpIndex > 0)
+            {
+                slnContents.Insert(gpIndex + 1, string.Format("		{{{0}}}.Debug|Any CPU.ActiveCfg = Debug|Any CPU", projectGuid));
+                slnContents.Insert(gpIndex + 2, string.Format("		{{{0}}}.Debug|Any CPU.Build.0 = Debug|Any CPU", projectGuid));
+            }
+
             if (System.IO.File.Exists(slnFilename))
             {
-                List<string> allLines = new List<string>();
-                allLines.AddRange(System.IO.File.ReadAllLines(slnFilename));
-
-                if ( FindIndexOf(allLines, SourceFolderName) < 0)
-                {
-                    var acecsprojh = FindIndexOf(allLines, "Assembly-CSharp-Editor.csproj");
-                    if (acecsprojh > 0)
-                    {
-                        var ProjectConfig = allLines[acecsprojh];
-                        var guid = PickGuid(ProjectConfig);
-                        var insertProj = string.Format("Project(\"{{{0}}}\") = \"{1}\", \"{1}.csproj\", \"{{{2}}}\"",  guid, SourceFolderName, projectGuid);
-                        allLines.Insert(acecsprojh + 2, insertProj);
-                        allLines.Insert(acecsprojh + 3, "EndProject");
-                    }
-
-                    var gpIndex = FindIndexOf(allLines, "GlobalSection(ProjectConfigurationPlatforms)");
-                    if (gpIndex > 0)
-                    {
-                        allLines.Insert(gpIndex + 1, string.Format("		{{{0}}}.Debug|Any CPU.ActiveCfg = Debug|Any CPU", projectGuid));
-                        allLines.Insert(gpIndex + 2, string.Format("		{{{0}}}.Debug|Any CPU.Build.0 = Debug|Any CPU", projectGuid));
-                    }
-
-                    if (System.IO.File.Exists(slnFilename))
-                    {
-                        System.IO.File.Delete(slnFname);
-                    }
-
-                    System.IO.File.WriteAllLines(slnFilename, allLines);
-                }
+                System.IO.File.Delete(slnFname);
             }
-            else
-            {
-                Console.WriteLine("WriteFail: " + slnFilename);
-            }
+            System.IO.File.WriteAllLines(slnFilename, slnContents);
 
             return 0;
         }
 
+        /// <summary>
+        /// Find String in StringList
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
         static int FindIndexOf(List<string> input, string str)
         {
             for(int i=0; i<input.Count; ++i)
@@ -127,59 +158,43 @@ namespace UnityCsProjGen
             }
             return output;
         }
-        
 
-        static bool Execute(string inputDir, string inputName, string templateFilename, string outputFilename)
+        static private bool ExecuteReplaceAction(string inputDir, string inputName, string templateFilename, string outputFilename)
         {
-            if (!System.IO.File.Exists(templateFilename))
+            var xmlReader = new System.Xml.XmlTextReader(templateFilename);
+            xmlReader.Namespaces = false;
+
+            System.Xml.XmlDocument xmlDocument = new System.Xml.XmlDocument();
+            xmlDocument.Load(xmlReader);
+
+            // delete xmlns attribute first
+            var xmlns = xmlDocument.DocumentElement.GetAttribute("xmlns");
+            xmlDocument.DocumentElement.RemoveAttribute("xmlns");
+
+            var filelist = FormatCompileIncludes(inputDir, inputName);
+
+            if (!ReplaceItemGroup(xmlDocument, filelist))
             {
-                Console.WriteLine( string.Format("Can't open {0}"), templateFilename);
                 return false;
             }
 
-            try
+            if (xmlns != null && xmlns.Length > 0)
             {
-                var xmlReader = new System.Xml.XmlTextReader(templateFilename);
-                xmlReader.Namespaces = false;
-
-                System.Xml.XmlDocument xmlDocument = new System.Xml.XmlDocument();
-                xmlDocument.Load(xmlReader);
-
-                // delete xmlns attribute first
-                var xmlns = xmlDocument.DocumentElement.GetAttribute("xmlns");
-                xmlDocument.DocumentElement.RemoveAttribute("xmlns");
-
-                var filelist = FormatCompileIncludes(inputDir, inputName);
-
-                if (!ReplaceItemGroup(xmlDocument, filelist))
-                {
-                    return false;
-                }
-
-                if (xmlns != null && xmlns.Length > 0)
-                {
-                    // append xmlns attribute
-                    xmlDocument.DocumentElement.SetAttribute("xmlns", xmlns);
-                }
-
-                var ProjectGuid = xmlDocument.SelectSingleNode("Project/PropertyGroup/ProjectGuid");
-                if (ProjectGuid != null)
-                {
-                    ProjectGuid.InnerText = "{" + projectGuid + "}";
-                }
-
-                if (System.IO.File.Exists(outputFilename))
-                {
-                    System.IO.File.Delete(outputFilename);
-                }
-
-                xmlDocument.Save(outputFilename);
+                xmlDocument.DocumentElement.SetAttribute("xmlns", xmlns);
             }
-            catch (Exception e)
+
+            var ProjectGuid = xmlDocument.SelectSingleNode("Project/PropertyGroup/ProjectGuid");
+            if (ProjectGuid != null)
             {
-                Console.WriteLine(e.Message);
-                return false;
+                ProjectGuid.InnerText = "{" + projectGuid + "}";
             }
+
+            if (System.IO.File.Exists(outputFilename))
+            {
+                System.IO.File.Delete(outputFilename);
+            }
+
+            xmlDocument.Save(outputFilename);
 
             return true;
         }
